ecbe0677246965133e536b06c7abb92dd4c5cb37
Reza Souri <rezasouri654@gmail.com>
2023-07-28 18:58:48 +0330

    axios method

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,39 @@
+import { common } from "@mui/material/colors";
 import axios from "axios";
 import { useEffect } from "react";
 
 const Home = () => {
-  useEffect(() => {
-    fetch("http://localhost:3001/", {
-      method: "GET",
+  const controller = new AbortController();
+  // useEffect(() => {
+  //   fetch("http://localhost:3001/", {
+  //     method: "GET",
+  //     signal: controller.signal,
+  //   })
+  //     .then((response) => {
+  //       console.log("response: ", response);
+  //       return response.json();
+  //     })
+  //     .then((responseJson) => {
+  //       console.log("responseJson: ", responseJson);
+  //     });
+  // }, []);
+  setTimeout(() => {
+    controller.abort();
+  }, 3000);
+
+  axios({
+    url: "http://localhost:3001",
+    method: "GET",
+    timeout: 3000,
+    timeoutErrorMessage: "error",
+    // headers:{},
+    // data:{}
+  })
+    .then((response) => {
+      console.log(response);
     })
-      .then((response) => {
-        console.log("response: ", response);
-        return response.json();
-      })
-      .then((responseJson) => {
-        console.log("responseJson: ", responseJson);
-      });
-  }, []);
-  
-  // axios({
-  //   url:"http://localhost:3001",
-  //   method:"GET",
-  //   // headers:{},
-  //   // data:{}
-  // }).then(response=>{
-  //   console.log(response);
-  // }).catch((e)=>{
-
-  // }).finally(()=>{
-
-  // })
+    .catch((e) => {})
+    .finally(() => {});
   return <div>Home</div>;
 };
 export default Home;
